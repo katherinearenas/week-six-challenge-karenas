e243b26095575f60d21649c15644f7ab735eed4f
Katherine <89894177+katherinearenas@users.noreply.github.com>
2024-07-08 15:53:55 -0500

    reworked html and js after running into errors

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,13 +1,29 @@
-const cityForecastEl = document.getElementById('#city-forecast');
-const fetchButton = document.getElementById('fetch-button');
+const fetchButton = document.getElementById('fetch-button'); 
+const cityForecastEl = document.getElementById('city-name-input');
 
-function getCoordinates() {
-  let coordinates = 'http://api.openweathermap.org/geo/1.0/direct?q=chicago,il,us&limit=10&appid=a096090ef2291b9381ff1519ce80d339'
-  return coordinates;
-}
-console.log(getCoordinates())
 
-function getApi() {
+
+function getCoordinates(event) {
+  event.preventDefault()
+  console.log(cityForecastEl.value)
+
+  let locationRequestURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+ cityForecastEl.value + '&limit=10&appid=a096090ef2291b9381ff1519ce80d339'
+
+  console.log(locationRequestURL)
+  fetch(locationRequestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      })
+    };
+
+
+
+function getApi(event) {
+  event.preventDefault()
+
   const requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=41.85003000&lon=-87.65005000&appid=a096090ef2291b9381ff1519ce80d339&units=imperial';
 
   fetch(requestUrl)
@@ -45,78 +61,5 @@ function getApi() {
       }
     });
 }
-fetchButton.addEventListener('click', getApi, getCoordinates);
 
-// const getRepoName = function () {
-//   const queryString = document.location.search;
-//   const repoName = queryString.split('=')[1];
-
-//   if (repoName) {
-//     repoNameEl.textContent = repoName;
-
-//     getRepoIssues(repoName);
-//   } else {
-//     document.location.replace('./index.html');
-//   }
-// };
-
-// const getRepoIssues = function (repo) {
-//   const apiUrl = `https://api.github.com/repos/${repo}/issues?direction=asc`;
-
-//   fetch(apiUrl).then(function (response) {
-//     if (response.ok) {
-//       response.json().then(function (data) {
-//         displayIssues(data);
-
-//         if (response.headers.get('Link')) {
-//           displayWarning(repo);
-//         }
-//       });
-//     } else {
-//       document.location.replace('./index.html');
-//     }
-//   });
-// };
-
-// const displayIssues = function (issues) {
-//   if (issues.length === 0) {
-//     issueContainerEl.textContent = 'This repo has no open issues!';
-//     return;
-//   }
-
-//   for (let issueObj of issues) {
-//     const issueEl = document.createElement('a');
-//     issueEl.classList = 'list-item flex-row justify-space-between align-center';
-//     issueEl.setAttribute('href', issueObj.html_url);
-//     issueEl.setAttribute('target', '_blank');
-
-//     const titleEl = document.createElement('span');
-//     titleEl.textContent = issueObj.title;
-//     issueEl.appendChild(titleEl);
-
-//     const typeEl = document.createElement('span');
-
-//     if (issueObj.pull_request) {
-//       typeEl.textContent = '(Pull request)';
-//     } else {
-//       typeEl.textContent = '(Issue)';
-//     }
-
-//     issueEl.appendChild(typeEl);
-
-//     issueContainerEl.appendChild(issueEl);
-//   }
-// };
-
-// const displayWarning = function (repo) {
-//   limitWarningEl.textContent = 'To see more than 30 issues, visit ';
-
-//   const linkEl = document.createElement('a');
-//   linkEl.textContent = 'GitHub.com';
-//   linkEl.setAttribute('href', `https://github.com/${repo}/issues`);
-//   linkEl.setAttribute('target', '_blank');
-
-//   limitWarningEl.appendChild(linkEl);
-// };
-
-// getRepoName();
+fetchButton.addEventListener('click', getCoordinates )
